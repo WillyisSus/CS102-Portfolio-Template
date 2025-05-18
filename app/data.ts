d95786d4259fdacad86da58063c72fabcd328263
Dingkemon <116236317+WillyisSus@users.noreply.github.com>
2025-05-18 16:28:59 +0700

    test again

--- app/data.ts
+++ app/data.ts
@@ -94,6 +94,12 @@ export const BLOG_POSTS: BlogPost[] = [
     link: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
     uid: 'blog-1',
   },
+  {
+    title: 'Ohmaigoto',
+    description: '💢💢💢💢💢',
+    link: '/blog/my-first-blog',
+    uid: 'blog-2',
+  }
 ]
 
 export const SOCIAL_LINKS: SocialLink[] = [
